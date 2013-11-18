d37ee2b748fb04d938e0800d494327383c4522e3
Pierre-Yves Aquilanti <sperif@gmail.com>
2013-11-18 14:06:28 -0600

    Format change with Scalariver.

--- Submitter.scala
+++ Submitter.scala
@@ -15,113 +15,110 @@ object Submitter {
     Usage: Submitter.scala config_filename
   """
 
-	def main(args: Array[String]) {
-		Console.err.println(Console.GREEN+"Launching submitter system."+Console.RESET)	
-		//check if a param file has been provided by the user
-		if(args.length!=1){
-			Console.err.println(Console.RED+"No input file provided."+Console.MAGENTA+usage+Console.RESET)
-			System.exit(1)
-		} else if(!fileExist(args(0))){ // or if file exist 
-			Console.err.println(Console.RED+"Input file "+args(0)+" provided does not exist."+Console.RESET)
-			System.exit(1)
-		}
+  def main(args: Array[String]) {
+    Console.err.println(Console.GREEN + "Launching submitter system." + Console.RESET)
+    //check if a param file has been provided by the user
+    if (args.length != 1) {
+      Console.err.println(Console.RED + "No input file provided." + Console.MAGENTA + usage + Console.RESET)
+      System.exit(1)
+    } else if (!fileExist(args(0))) { // or if file exist
+      Console.err.println(Console.RED + "Input file " + args(0) + " provided does not exist." + Console.RESET)
+      System.exit(1)
+    }
 
-		//get file config map
-		Console.err.println(Console.GREEN+"Getting config file : "+Console.RESET+args(0))	
-		val configMap = ConfigProvider.fileToMap(args(0))
+    //get file config map
+    Console.err.println(Console.GREEN + "Getting config file : " + Console.RESET + args(0))
+    val configMap = ConfigProvider.fileToMap(args(0))
 
-		//is the file valid
-		if(!ConfigProvider.isConfigMapValid(configMap,"COMMAND")){
-			Console.err.println(Console.RED+"Input file "+args(0)+" does not contain a COMMAND \nconfiguration line or this line is invalid."+Console.RESET)
-			System.exit(1)
-		} else Console.err.println(Console.GREEN+"Launching provided config file is valid."+Console.RESET)	
-		
+    //is the file valid
+    if (!ConfigProvider.isConfigMapValid(configMap, "COMMAND")) {
+      Console.err.println(Console.RED + "Input file " + args(0) + " does not contain a COMMAND \nconfiguration line or this line is invalid." + Console.RESET)
+      System.exit(1)
+    } else Console.err.println(Console.GREEN + "Launching provided config file is valid." + Console.RESET)
 
-		Console.err.println(Console.GREEN+"Extracting informations from config file."+Console.RESET)
-		//Get the commands list with list of args
-		val commandList = ConfigProvider.configToList("COMMAND",configMap)
-    
+    Console.err.println(Console.GREEN + "Extracting informations from config file." + Console.RESET)
+    //Get the commands list with list of args
+    val commandList = ConfigProvider.configToList("COMMAND", configMap)
+
     // compute the resulting command
-    Console.err.println(Console.GREEN+"Constructing command list."+Console.RESET)
-		val result = ConfigProvider.listToCommandList(configMap("COMMAND"), commandList)
-    Console.err.println(Console.GREEN+"Outputing command list to standart output."+Console.RESET)
+    Console.err.println(Console.GREEN + "Constructing command list." + Console.RESET)
+    val result = ConfigProvider.listToCommandList(configMap("COMMAND"), commandList)
+    Console.err.println(Console.GREEN + "Outputing command list to standart output." + Console.RESET)
     result.foreach(println _)
   }
 
-
-  def fileExist(file: String):Boolean = (new File(file)).isFile
-
+  def fileExist(file: String): Boolean = (new File(file)).isFile
 
   object ConfigProvider {
-  	//check if a parameter map is valid
-  	def isConfigMapValid(map: Map[String,String], command: String):Boolean = {
-  		if(map.contains(command)){
-  			val commandMap=extractCommandToMap(map(command))
-  			//if the set is empty if yes then we consider that the parameter map
-  			//is valid because all commands contained in the command string
-  			//are contained in the parameter map, nothing is missing
-  			(commandMap.keySet &~ map.keySet).isEmpty
-  		} else false
-  	}
+    //check if a parameter map is valid
+    def isConfigMapValid(map: Map[String, String], command: String): Boolean = {
+      if (map.contains(command)) {
+        val commandMap = extractCommandToMap(map(command))
+        //if the set is empty if yes then we consider that the parameter map
+        //is valid because all commands contained in the command string
+        //are contained in the parameter map, nothing is missing
+        (commandMap.keySet &~ map.keySet).isEmpty
+      } else false
+    }
 
-  	//check if config file is valid
-  	def isConfigFileValid(filePath: String, command: String):Boolean = {
-  		val map = fileToMap(filePath)
-  		isConfigMapValid(map,command)
-  	}
+    //check if config file is valid
+    def isConfigFileValid(filePath: String, command: String): Boolean = {
+      val map = fileToMap(filePath)
+      isConfigMapValid(map, command)
+    }
 
-      //transform a file to a map
-		def fileToMap(filePath: String):Map[String,String] = {
-			val regex = """^(_?[a-zA-Z0-9]\w*_?)\s*=\s*\"(.*)\"#?.*$""".r
-			val comArgs = mutable.Map[String, String]()
+    //transform a file to a map
+    def fileToMap(filePath: String): Map[String, String] = {
+      val regex = """^(_?[a-zA-Z0-9]\w*_?)\s*=\s*\"(.*)\"#?.*$""".r
+      val comArgs = mutable.Map[String, String]()
 
-			for(line <-Source.fromFile(filePath).getLines)
-			    for(regex(c,a) <- regex findAllIn line)
-      				comArgs(c)=a
-  	 	comArgs.toMap
-		}
-		  
-		  //transform a string containing strings such as ${priority:command} to a map
-		def extractCommandToMap(command: String):Map[String, Int] = {
-  		val regex = """\$\{([0-9]+)?:?([\w]+)\}""".r
-  		val com = mutable.Map[String, Int]()
+      for (line <- Source.fromFile(filePath).getLines)
+        for (regex(c, a) <- regex findAllIn line)
+          comArgs(c) = a
+      comArgs.toMap
+    }
 
-  		//get the commands, remove all blank characters 
-  		for (regex(c,b) <- regex findAllIn command.replaceAll("""\s""", ""))
-  			com(b)= if(c!=null && c.toInt>0 && c.toInt<=99) c.toInt else 99
-  		com.toMap
-  	}
+    //transform a string containing strings such as ${priority:command} to a map
+    def extractCommandToMap(command: String): Map[String, Int] = {
+      val regex = """\$\{([0-9]+)?:?([\w]+)\}""".r
+      val com = mutable.Map[String, Int]()
 
-      //transform a map to list
-  	def configToList(command: String,configMap:Map[String,String]):List[(String,List[String])] = {
-		 //get the COMMAND config that contain the final command in value sorted order
-  		val commandList = extractCommandToMap(configMap(command)).toList.sortBy(_._2).reverse
-  		val configMapList:Map[String,List[String]] = for{(confk,confv) <- configMap.filterKeys(configMap.keySet & commandList.toMap.keySet)}
-		 											yield (confk -> confv.split("[,:;]").toList)
+      //get the commands, remove all blank characters
+      for (regex(c, b) <- regex findAllIn command.replaceAll("""\s""", ""))
+        com(b) = if (c != null && c.toInt > 0 && c.toInt <= 99) c.toInt else 99
+      com.toMap
+    }
 
-		  for( com <- commandList) yield (com._1 -> configMapList(com._1))
-  	}
+    //transform a map to list
+    def configToList(command: String, configMap: Map[String, String]): List[(String, List[String])] = {
+      //get the COMMAND config that contain the final command in value sorted order
+      val commandList = extractCommandToMap(configMap(command)).toList.sortBy(_._2).reverse
+      val configMapList: Map[String, List[String]] = for { (confk, confv) <- configMap.filterKeys(configMap.keySet & commandList.toMap.keySet) }
+        yield (confk -> confv.split("[,:;]").toList)
 
-      //take a list of lists containing the keyes and values, we iterate over the values in the list order to replace the keys
-      //by the values in string command (ok that's not really clear but still that's what it does...)
-  	def listToCommandList(command:String, list: List[(String,List[String])]):List[String] = {
-      val batch:List[String] = if(!list.isEmpty) replaceToList(command,list.head._1,list.head._2) else List[String](command)
+      for (com <- commandList) yield (com._1 -> configMapList(com._1))
+    }
 
-      if(!list.tail.isEmpty) {
-        val ll = for(com <- batch) yield listToCommandList(com,list.tail)
-        ll.foldLeft(List[String]())((a,b) => a ::: b)
-      }else batch        
-  	}
+    //take a list of lists containing the keyes and values, we iterate over the values in the list order to replace the keys
+    //by the values in string command (ok that's not really clear but still that's what it does...)
+    def listToCommandList(command: String, list: List[(String, List[String])]): List[String] = {
+      val batch: List[String] = if (!list.isEmpty) replaceToList(command, list.head._1, list.head._2) else List[String](command)
+
+      if (!list.tail.isEmpty) {
+        val ll = for (com <- batch) yield listToCommandList(com, list.tail)
+        ll.foldLeft(List[String]())((a, b) => a ::: b)
+      } else batch
+    }
 
     //for one command and one list of values, replace the keyword in command for each value (iteration)
     //return a list of replaced commands
-    def replaceToList(command: String, keyword: String, values:List[String]):List[String] = 
-        for{value <- values} yield replaceCommand(command,keyword,value) 
+    def replaceToList(command: String, keyword: String, values: List[String]): List[String] =
+      for { value <- values } yield replaceCommand(command, keyword, value)
 
     //replace a keyword by a value in string command depending on regex pattern
-    def replaceCommand(command:String, key:String, value:String):String = {
-      val regex = new Regex("\\$\\{([0-9]+)?:?"+key+"\\}") 
-      regex replaceAllIn ( command, value)
+    def replaceCommand(command: String, key: String, value: String): String = {
+      val regex = new Regex("\\$\\{([0-9]+)?:?" + key + "\\}")
+      regex replaceAllIn (command, value)
     }
   }
 }
